ad6f7026b7fe6c4e370e3bc9ec91f72db16c0087
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-02 23:47:28 +0000

    Refactored module

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,32 @@
 const request = require('request');
 
-const args = process.argv.splice(2);
-const breed = args[0];
+// const args = process.argv.splice(2);
+// const breed = args[0];
 
-const path = `https://api.thecatapi.com/v1/breeds/search?q=` + breed;
 
-let data;
 
-// const fetchHandle = request(path, (error, response, body) => {
-//   data = body;
-//   fs.writeFile(file, data, err => {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//   })
-//   return data;
-// });
+const fetchBreedDescription = function(breedName, callback) {
+  const path = `https://api.thecatapi.com/v1/breeds/search?q=` + breedName;
 
-// return fetchHandle;
+  let data;
 
-request(path, (error, response, body) => {
-  if (error) {
-    return console.error(`Sorry, that cat is not found`, error);
-  }
-  data = JSON.parse(body);
-  console.log(`Type of body: ${typeof data}`, data[0].description);
-});
+  request(path, (error, response, body) => {
+    if (error) {
+       callback(error, null);
+    }
+    //console.error('error: ', error);
+    console.log('statusCode:', response && response.statusCode);
+    data = JSON.parse(body);
+    // console.log(`Type of body: ${typeof data}`, data[0].description);
+    //console.log(`Type of body: ${typeof data}`, data);
+    if(data === []) {
+      callback(error, null);
+    } else {  
+      console.log(data);
+      // callback(null, data[0].description);
+    }
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
