7683fee4d151d8b5b3c5be5706d1b6675b9b363f
Vlad-Lab319 <solo15mag@gmail.com>
2021-12-03 00:05:52 +0000

    unit test applied

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,5 @@
 const request = require('request');
 
-// const args = process.argv.splice(2);
-// const breed = args[0];
-
-
-
 const fetchBreedDescription = function(breedName, callback) {
   const path = `https://api.thecatapi.com/v1/breeds/search?q=` + breedName;
 
@@ -12,18 +7,14 @@ const fetchBreedDescription = function(breedName, callback) {
 
   request(path, (error, response, body) => {
     if (error) {
-       callback(error, null);
-    }
-    //console.error('error: ', error);
-    console.log('statusCode:', response && response.statusCode);
-    data = JSON.parse(body);
-    // console.log(`Type of body: ${typeof data}`, data[0].description);
-    //console.log(`Type of body: ${typeof data}`, data);
-    if(data === []) {
       callback(error, null);
-    } else {  
-      console.log(data);
-      // callback(null, data[0].description);
+    }
+    // console.log('statusCode:', response && response.statusCode);
+    data = JSON.parse(body);
+    if (data[0].description) {
+      callback(null, data[0].description);
+    } else {
+      callback(error, null);
     }
   });
 
